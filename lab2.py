f21ef4b573d4b8fce4d7f91831fdaee0d1d5917c
BaconPancakez <Baconnpancakezz@gmail.com>
2024-08-29 22:44:58 +0800

    Insert

---
+++ lab2.py
@@ -0,0 +1,65 @@
+# TODO : 1. There are two players in the game. Initialize their names as string
+# Add you code of TODO 1 here
+
+player1 = input("Insert player1 name:")
+player2 = input("Insert player2 name:")
+
+# TODO : 2. There are two layers in the sea. Initialize the integer variable layer
+# Add you code of TODO 2 here
+
+layer = 2
+
+# TODO : 3. The battleship board is 10 by 10 dimension. Initialize the integer variables
+# width and height
+# Add you code of TODO 3 here
+
+width = 10
+height = 10
+
+# TODO : 4. Initialize a boolean variable that will be used to indicate
+# whether user input is valid or not, two boolean variables hit and miss that indicate
+# whether the ship is hit or missed
+# Add you code of TODO 4 here
+
+valid = False
+hit = False
+miss = False
+
+# TODO : 5. The ships have only two orientation, either vertical or horizontal.
+# Initialize a variable as ship orientation
+# Add you code of TODO 5 here
+
+ori = 'v'
+
+# TODO : 6. The coordinates of where to put ship or where to hit on the board have a
+# specific format. Create a string variable as coordinates
+# Add you code of TODO 6 here
+
+coor = "A,4,1"
+
+# TODO : 7. There are only two types of ships. Initialize two string variables as ship names
+# Add you code of TODO 7 here
+
+ship1 = 'Submarine'
+ship2 = 'Carrier'
+
+
+# TODO : 8. Initialize a string variable holding a welcome message that can be displayed to user
+# Add you code of TODO 8 here
+welmes = ("Welcome to Battleships! Hit ENTER to continue.\n\
+Please enter coordinate of the attack center point in following this format (row,col,depth). E.g " + coor + "\n\
+Note: depth = 0 represents the subsea layer, and depth = 1 represents the surface level.\n")
+print(player1, player2)
+print(layer)
+print(width, height)
+print(valid, hit, miss)
+print(ori)
+print(coor)
+print(ship1, ship2)
+print(welmes)
+
+# TODO : 9. Take user input for attack coordinates and display the result.
+# Add you code of TODO 9 here
+
+coor = input("Enter coordinates: ")
+print("Hit at area centering " + coor)
---
+++ lab3.py
@@ -0,0 +1,100 @@
+from tabnanny import check
+from tkinter import Y
+
+board_size = 10
+valid_orientations = ['horizontal', 'vertical']
+valid = False
+
+
+# Exercise 1: Basic Conditional Statements
+x, y = 5, 8
+# TODO : 1. Check if coordinates are within the valid range
+# Add you code of TODO 1 here
+if (1 <= x <= board_size and 1 <= y <= board_size):
+    valid = True
+else:
+    valid = False
+
+print(f"Coordinates ({x}, {y}) are valid: {valid}")
+
+#One more example
+x, y = 10, -1
+# TODO : 2. Check if coordinates are within the valid range
+# Copy your code of TODO 1 here to test on x, y = 10, -1
+if (1 <= x <= board_size and 1 <= y <= board_size):
+    valid = True
+else:
+    valid = False
+
+print(f"Coordinates ({x}, {y}) are valid: {valid}")
+
+
+
+
+# Exercise 2: Conditional with Logical Operators
+x, y, orientation = 4, 6, 'horizontal'
+
+# TODO : 3. Validate user input for coordinates and orientation
+# Add you code of TODO 3 here
+if ((1 <= x <= board_size and 1 <= y <= board_size)) and orientation in valid_orientations:
+    valid = True
+else:
+    valid = False
+
+print(f"Coordinates ({x}, {y}, {orientation}) are valid: {valid}")
+
+#One more example
+x, y, orientation = 11, 3, 'diagonal'
+
+# TODO : 4. Validate user input for coordinates and orientation
+# Copy your code of TODO 3 here to test on x, y, orientation = 11, 3, 'diagonal'
+
+if ((1 <= x <= board_size and 1 <= y <= board_size)) and orientation in valid_orientations:
+    valid = True
+else:
+    valid = False
+
+print(f"Coordinates ({x}, {y}, {orientation}) are valid: {valid}")
+
+
+
+
+
+# Exercise 3: Nested Conditionals
+x, y, ship_length, orientation = 3, 5, 4, 'horizontal'
+# TODO : 5. Validate the placement of a ship
+# Add you code of TODO 5 here
+if orientation.lower() == 'horizontal':
+    if (1 <= (x + ship_length) <= board_size and 1 <= y <= board_size):
+        valid = True
+    else:
+        valid = False
+elif orientation.lower() == 'vertical':
+    if (1 <= x <= board_size and 1 <= (y + ship_length) <= board_size):
+        valid = True
+    else:
+        valid = False
+else:
+    valid = False
+
+print(f"Coordinates ({x}, {y}, {ship_length}, {orientation}) are valid: {valid}")
+
+#One more example
+x, y, ship_length, orientation = 7, 7, 4, 'vertical'
+# TODO : 6. Validate the placement of a ship
+# Copy your code of TODO 5 here to test on x, y, ship_length, orientation = 7, 7, 4, 'vertical'
+
+if orientation.lower() == 'horizontal':
+    if (1 <= (x + ship_length) <= board_size and 1 <= y <= board_size):
+        valid = True
+    else:
+        valid = False
+elif orientation.lower() == 'vertical':
+    if (1 <= x <= board_size and 1 <= (y + ship_length) <= board_size):
+        valid = True
+    else:
+        valid = False
+else:
+    valid = False
+
+print(f"Coordinates ({x}, {y}, {ship_length}, {orientation}) are valid: {valid}")
