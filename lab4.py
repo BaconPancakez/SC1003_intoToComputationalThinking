4ac1d3d88f45f831a18fcb67d89f1abaadde3ea7
BaconPancakez <Baconnpancakezz@gmail.com>
2024-08-30 17:24:50 +0800

    Insert

---
+++ lab4.py
@@ -0,0 +1,13 @@
+board_size = 10
+# TODO : 1. Initialize a board_sizexboard_size game board with allncells set to 0 (empty)
+# Add you code of TODO 1 here
+
+
+# TODO : 2.While loop to repeatedly ask for valid attack coordinates
+# Add you code of TODO 2 here
+
+
+
+#4 – Iteration
+# TODO : 3. For loop to iterate through each row and column of the board
+# Add you code of TODO 3 here
